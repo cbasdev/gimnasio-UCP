9f00e3c4d9f95cbcc077a411b0fd04bee06c1a5c
sebasttianvelez <sebasttianvelez@gmail.com>
2020-10-14 22:21:46 -0500

    Test

--- frontend/src/app/pages/Plans/Plans.js
+++ frontend/src/app/pages/Plans/Plans.js
@@ -1,14 +1,11 @@
 import React from 'react'
 
 export default function Plans() {
+  const number = 30
   return (
     <div>
       <h2>Prueba de planes</h2>
-      <ul>
-        <li> Plan 1: $30000</li>
-        <li> Plan 2: $10000</li>
-        <li> Plan 3: $50000</li>
-      </ul>
+      <ul>{number}</ul>
     </div>
   )
 }
