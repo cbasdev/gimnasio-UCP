b8898ba652b2be846bc6352fe679bf133e923b1f
sebasttianvelez <sebasttianvelez@gmail.com>
2020-10-14 22:18:05 -0500

    planes de entrenamiento

--- frontend/src/app/pages/Plans/Plans.js
+++ frontend/src/app/pages/Plans/Plans.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export default function Plans() {
-  return <div>pagina de planes...test</div>
+  return (
+    <div>
+      <h2>Prueba de planes</h2>
+      <ul>
+        <li> Plan 1: $30000</li>
+        <li> Plan 2: $10000</li>
+        <li> Plan 3: $50000</li>
+      </ul>
+    </div>
+  )
 }
